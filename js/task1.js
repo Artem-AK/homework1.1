42832c2fed13277d71990569ecd39bb0571188a9
Artem <kozhin345@gmail.com>
2021-05-23 16:12:31 +0300

    init

---
+++ js/task1.js
@@ -0,0 +1,12 @@
+let var1 = prompt("Введите первое значение:"); 
+let var2 = prompt("Введите второе значение: ");
+
+let result = var1 - var2;
+
+alert("Результат: " + result);
+
+let names = prompt("Введите имя: ");
+let surname = prompt("Введите фамилию: ");
+let patronymic = prompt("Введиет отчество: ");
+
+alert("Имя: " + names + " Фамилия: " + surname + " Отчество: " + patronymic);
